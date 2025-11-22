70bf45dd2514818f7c5cd2e168d13e84baff9032
tapo <hypergag@gmail.com>
2025-11-22 14:26:37 +0400

    0.1.0
    initial commit.

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# CHANGELOG.md
+
+---
+
+## [0.1.0] - 22.11.2025
+- Started the project
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MiniPollApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
